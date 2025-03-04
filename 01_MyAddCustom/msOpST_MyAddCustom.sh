438ed177e01cfe1fe29b8cdd4f9d35c98f095801
Eliwii_Keeya <Eliwii_Keeya@163.com>
2025-03-04 22:35:15 +0800

    feat(Operator): add generation scripts and ST test scripts
    Add generator scripts to create operator development projects.
    Add an ST test script to test that the operator is functioning correctly.

---
+++ 01_MyAddCustom/msOpST_MyAddCustom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export DDK_PATH=${ASCEND_TOOLKIT_HOME}
+export NPU_HOST_LIB=${ASCEND_TOOLKIT_HOME}/aarch64-linux/devlib
+echo DDK_PATH: ${DDK_PATH}
+echo NPU_HOST_LIB: ${NPU_HOST_LIB}
+${ASCEND_TOOLKIT_HOME}/python/site-packages/bin/msopst run \
+    -i 01_MyAddCustom/msOpST_MyAddCustom.json \
+    -soc Ascend910B1 \
+    -out MyAddCustomST
